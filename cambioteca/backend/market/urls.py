45821bed2a92cd88fe4d85d7de7feafeb2c5daf2
Nicolás Rebolledo <124644179+Nicofelipe@users.noreply.github.com>
2025-10-25 20:47:36 -0300

    Delete cambioteca directory

--- cambioteca/backend/market/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import LibroViewSet
-
-router = DefaultRouter()
-router.register(r'libros', LibroViewSet, basename='libros')
-urlpatterns = router.urls
--- cambioteca/mobile/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true//,
-  ///apiUrl: 'https://tu-api.en-render.com', // cámbialo en producción
-};
